82d0dfc95fdc224b2ac2ed8f4bc3758235ff840c
TakahashiMasaya <ma.taka.ic@icloud.com>
2023-12-31 10:52:11 +0900

    deleted unnecessary files

--- middleware/test.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('this is middleware')
-  // return createError({ statusCode: 404, statusMessage: 'Page Not Found', fatal: true })
- return showError({ statusCode: 403, message: '認証されていません'})
-  // return abortNavigation(
-  //   showError({ statusCode: 403, message: '認証されていません'})
-  // )
-})
